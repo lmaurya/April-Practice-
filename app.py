800588e748ce028d190996ef3c73e8488635b9f0
root <lokeshmaurya30@gmail.com>
2020-04-04 09:42:03 +0000

    Adding to check branches

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 print("Hello world")
 Print ("This is my third line ")
 name lokesh
-to become a perfect coder 
+to become a perfect coder
+To verify branch in dev 
+Check is any update go to master branch or not  
  
