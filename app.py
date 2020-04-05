f1acc65801341f4df6f14c717a25ebc0c3a3bce9
lmaurya <63008169+lmaurya@users.noreply.github.com>
2020-04-05 20:28:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ name lokesh
 to become a perfect coder
 To verify branch in dev 
 Check is any update go to master branch or not  
- 
+ testing jenkins 
+ to know pull scm will work after 2min or not 
